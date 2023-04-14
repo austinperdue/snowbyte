7925ee38ffd21d81945c757a59fa434a06b0ce2a
austinperdue <47020337+austinperdue@users.noreply.github.com>
2023-04-13 23:12:48 -0400

    Initial setup
    Installed font awesome and set up depencies, cleaned up a few files

---
+++ src/components/Navbar.js
@@ -0,0 +1,46 @@
+import React, {useState} from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope, faHippo } from '@fortawesome/free-solid-svg-icons'
+import { Link } from 'react-router-dom';
+import './Navbar.css';
+
+function Navbar() {
+    const [click, setClick] = useState(false);
+
+    const handleClick = () => setClick(!click);
+    const closeMobileMenu = () => setClick(false);
+    return (
+        <>
+            <nav className='navbar'>
+                <div className='navbar-container'>
+                    <Link to="/" className='navbar-logo'>
+                        TEST <FontAwesomeIcon icon={faHippo} />
+                    </Link>
+                    <div className='menu-icon'>
+                        <FontAwesomeIcon icon={faEnvelope} onClick={handleClick} />
+                    </div>
+                    <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                        <li className='nav-item'>
+                            <Link to='/' className='nav-links' onclick={closeMobileMenu}>
+                                Home
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/services' className='nav-links' onclick={closeMobileMenu}>
+                                Services
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/products' className='nav-links' onclick={closeMobileMenu}>
+                                Products
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </>
+    );
+}
+
+export default Navbar;
+
