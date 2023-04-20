f4d5a65ad6eeb1fc0831cf3abdd87cfbaf4c04cf
austinperdue <47020337+austinperdue@users.noreply.github.com>
2023-04-20 13:02:07 -0400

    Backend
    Added environmental variables for development and production for MYSQL server. Created user table in database. Signup page is currently working adding test  user to database

--- src/SignUp.js
+++ src/SignUp.js
@@ -1,11 +1,31 @@
 import React from 'react';
-import SignUpForm from './components/SignUpForm';
+import SignUpForm from './components/SignUpForm.js';
 
 function SignUp() {
-    const onSubmit = (data) => {
-        console.log(data);
-        // Handle form submission here
-    };
+    const onSubmit = async (data) => {
+        try {
+          console.log('REACT_APP_API_URL:', process.env.REACT_APP_API_URL); // Add this line
+      
+          const response = await fetch(`${process.env.REACT_APP_API_URL}/api/auth/signup`, {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+          });
+      
+          const result = await response.json();
+          if (response.ok) {
+            console.log('User created successfully:', result);
+            // Handle successful user registration (e.g., show a success message, navigate to the sign-in page, etc.)
+          } else {
+            console.error('User creation failed:', result.message);
+            // Show an error message
+          }
+        } catch (error) {
+          console.error('Error during user creation:', error);
+        }
+      };
 
     return (
         <div style={{ display: 'flex', minHeight: '75vh', flexDirection: 'column' }}>
--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 const cors = require('cors');
+const crypto = require('crypto'); // for random string generation
+const bcrypt = require('bcrypt'); // for password hashing
 
 const app = express();
 
@@ -8,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 // MySQL database connection
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host: 'snowbyte-db-do-user-13916321-0.b.db.ondigitalocean.com', 
   user: 'doadmin', 
   password: 'AVNS_Cu0RIaivJmdP8HEI64E',
@@ -16,13 +18,7 @@ const db = mysql.createConnection({
   port: 25060, 
 });
 
-db.connect((error) => {
-  if (error) {
-    console.error('Error connecting to the MySQL database:', error);
-    return;
-  }
-  console.log('Connected to the MySQL database');
-});
+console.log('Connected to the MySQL database');
 
 // API endpoint for user login
 app.post('/api/auth/signin', async (req, res) => {
@@ -36,7 +32,33 @@ app.post('/api/auth/signin', async (req, res) => {
   res.status(200).json({ message: 'Login successful' });
 });
 
-const PORT = process.env.PORT || 25060;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+});
+
+// auth for signup
+app.post('/api/auth/signup', async (req, res) => {
+  const { firstName, lastName, email, password } = req.body;
+
+  // Generate a random guest_id
+  const guest_id = crypto.randomBytes(16).toString('hex');
+
+  // Check if the email address already exists in the database
+  const checkEmailQuery = 'SELECT * FROM users WHERE email_address = ?';
+  const [emailRows] = await db.execute(checkEmailQuery, [email]);
+
+  if (emailRows.length > 0) {
+    return res.status(400).json({ message: 'Email address already in use' });
+  }
+
+  // Hash the password
+  const salt = bcrypt.genSaltSync(10);
+  const hashedPassword = bcrypt.hashSync(password, salt);
+
+  // Insert the new user into the database
+  const insertUserQuery = 'INSERT INTO users (guest_id, first_name, last_name, email_address, password) VALUES (?, ?, ?, ?, ?)';
+  await db.execute(insertUserQuery, [guest_id, firstName, lastName, email, hashedPassword]);
+
+  res.status(201).json({ message: 'User created successfully', guest_id });
 });
